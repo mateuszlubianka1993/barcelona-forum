ee7d5aeb3fd32c50c4266a36d5d9ff7c9790b337
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-12-16 15:02:36 +0100

    Add is-logged helper function and protect routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isLogged = require('../utils/is-logged');
 
 const router = express.Router();
 
-router.get('/add-news', adminController.getAddNews);
-router.post('/add-news', adminController.postAddNews);
-router.get('/news-list', adminController.getNewsList);
-router.get('/edit-news/:newsId', adminController.getEditNews);
-router.post('/edit-news', adminController.postEditNews);
-router.post('/delete-news', adminController.postDeleteNews);
+router.get('/add-news', isLogged, adminController.getAddNews);
+router.post('/add-news', isLogged, adminController.postAddNews);
+router.get('/news-list', isLogged, adminController.getNewsList);
+router.get('/edit-news/:newsId', isLogged, adminController.getEditNews);
+router.post('/edit-news', isLogged, adminController.postEditNews);
+router.post('/delete-news', isLogged, adminController.postDeleteNews);
 
 module.exports = router;
--- routes/forum.js
+++ routes/forum.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
 const forumController = require('../controllers/forum');
+const isLogged = require('../utils/is-logged');
 
 const router = express.Router();
 
 router.get('/', forumController.getHome);
 router.get('/news-list', forumController.getNewsList);
 router.get('/news-list/:newsId', forumController.getNewsItem);
-router.post('/favouriteNewsList', forumController.postFavouriteNews);
-router.get('/user/favourite-news-list', forumController.getFavouriteNewsList);
-router.post('/delete-favourite-news', forumController.postDeleteFavouriteItem);
+router.post('/favouriteNewsList', isLogged, forumController.postFavouriteNews);
+router.get('/user/favourite-news-list', isLogged, forumController.getFavouriteNewsList);
+router.post('/delete-favourite-news', isLogged, forumController.postDeleteFavouriteItem);
 
 module.exports = router;
