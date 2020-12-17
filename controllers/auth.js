d395ba45552e8b4117e7e9c9ee85e3e1c9b99ebf
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-12-17 11:58:36 +0100

    Add change/new pasword pages, add change password functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
@@ -98,6 +100,92 @@ exports.postSignUp = (req, res) => {
 							console.log(err);
 						});
 				});
+		})
+		.catch(err => {
+			console.log(err);
+		});
+};
+
+exports.getChangePassword = (req, res) => {
+	res.render('auth/change-password', {
+		path: '/change-password',
+		pageTitle: 'Change password Page',
+		errorMessage: req.flash('errorMessage')
+	});
+};
+
+exports.postChangePassword = (req, res) => {
+	crypto.randomBytes(32, (err, buffer) => {
+		if(err) {
+			console.log(err);
+			return res.redirect('/change-password');
+		}
+		const token = buffer.toString('hex');
+		const email = req.body.email;
+		User.findOne({email: email})
+			.then(user => {
+				if(!user) {
+					req.flash('errorMessage', 'User with this email address not found.');
+					return res.redirect('/change-password');
+				}
+				user.resetToken = token;
+				user.resetTokenExpiration = Date.now() + 3600000;
+				return user.save();
+			})
+			.then(() => {
+				res.redirect('/');
+				transporter.sendMail({
+					to: email,
+					from: 'lubiankamateusz@gmail.com',
+					subject: 'Change password.',
+					html: `
+					<h1>Change password.</h1>
+					<p>We received a request to change the password.</p>
+					<p>To change your password, click on this <a href="http://localhost:3000/change-password/${token}">link</a>.</p>
+				`
+				});
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	});
+};
+
+exports.getNewPassword = (req, res) => {
+	const token = req.params.token;
+	User.findOne({resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
+		.then(user => {
+			res.render('auth/new-password', {
+				path: '/new-password',
+				pageTitle: 'New password Page',
+				errorMessage: req.flash('errorMessage'),
+				userId: user._id.toString(),
+				passwordToken: token
+			});
+		})
+		.catch(err => {
+			console.log(err);
+		});
+};
+
+exports.postNewPassword = (req, res) => {
+	const userId = req.body.userId;
+	const password = req.body.password;
+	const token = req.body.passwordToken;
+	let updatedUser;
+	User.findOne({_id: userId, resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
+		.then(user => {
+			updatedUser = user;
+			return bcrypt.hash(password, 12);
+		})
+		.then(hashedPass => {
+			updatedUser.password = hashedPass;
+			updatedUser.resetToken = undefined;
+			updatedUser.resetTokenExpiration = undefined;
+			return updatedUser.save();
+		})
+		.then(() => {
+			res.redirect('/login');
 		})
 		.catch(err => {
 			console.log(err);
--- routes/auth.js
+++ routes/auth.js
@@ -9,5 +9,9 @@ router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.get('/signup', authController.getSignUp);
 router.post('/signup', authController.postSignUp);
+router.get('/change-password', authController.getChangePassword);
+router.post('/change-password', authController.postChangePassword);
+router.get('/change-password/:token', authController.getNewPassword);
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
