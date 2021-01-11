f414d7629bb3f7466cc2a387c39fe5596eb320c7
mateuszlubianka1993 <lubiankamateusz@gmail.com>
2021-01-11 12:07:01 +0100

    App js error handle

--- app.js
+++ app.js
@@ -45,11 +45,14 @@ app.use((req, res, next) => {
 	}
 	User.findById(req.session.user._id)
 		.then(user => {
+			if(!user) {
+				return next();
+			}
 			req.user = user;
 			next();
 		})
 		.catch(err => {
-			console.log(err);
+			throw new Error(err);
 		});
 });
 
