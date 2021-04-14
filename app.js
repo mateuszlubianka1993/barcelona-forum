b5445d7264e8338a7f7044f367458ef9c286eae7
mateuszlubianka1993 <lubiankamateusz@gmail.com>
2021-04-14 21:33:01 +0200

    Prepare to deploy

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.use(errorController.get404Page);
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => {
-		app.listen(3000);
+		app.listen(process.env.PORT || 3000);
 	})
 	.catch(err => {
 		console.log(err);
